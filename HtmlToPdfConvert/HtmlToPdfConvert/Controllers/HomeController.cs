daf071ac7e787729414df8f68f8d15e4637df8fb
Songül Keskin <songulkeskin99@gmail.com>
2022-11-29 14:49:58 +0300

    htmlToPdf

---
+++ HtmlToPdfConvert/HtmlToPdfConvert/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using HtmlToPdfConvert.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SelectPdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlToPdfConvert.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     public IActionResult GeneratePdf(string html)
+        {
+
+            html = html.Replace("StrTag", "<").Replace("EndTag", ">");
+            HtmlToPdf ohtmlToPdf = new HtmlToPdf();
+            PdfDocument pdfDocument = ohtmlToPdf.ConvertHtmlString(html);
+            byte[] pdf = pdfDocument.Save();
+            pdfDocument.Close();
+            return File(
+
+                pdf,
+                "application/pdf",
+                "StudentList.pdf"
+                ) ; 
+        }
+    }
+}
